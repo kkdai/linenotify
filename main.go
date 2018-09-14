d699e459ef62875c0cd3b436c9d64324c99aa36e
Evan Lin <evanslin@gmail.com>
2018-09-14 22:20:05 +0800

    update

--- main.go
+++ main.go
@@ -54,7 +54,7 @@ func authHandler(w http.ResponseWriter, r *http.Request) {
 			log.Fatal(err)
 		}
 	}
-	t, err := template.New("webpage").ParseFiles("tmp/auth.html")
+	t, err := template.New("webpage").ParseFiles("tmp/auth.html", nil)
 	check(err)
 	noItems := struct {
 		ClientID    string
@@ -67,27 +67,3 @@ func authHandler(w http.ResponseWriter, r *http.Request) {
 	err = t.Execute(w, noItems)
 	check(err)
 }
-
-const tpl2 = `
-<!DOCTYPE html>
-<html lang="tw">
-    <head>
-        <title></title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <script>
-        function oAuth2() {
-            var URL = 'https://notify-bot.line.me/oauth/authorize?';
-            URL += 'response_type=code';
-            URL += '&client_id={{.ClientID}}';
-            URL += '&redirect_uri={{.CallbackURL}}';
-            URL += '&scope=notify';
-            URL += '&state=NO_STATE';
-            window.location.href = URL;
-        }
-    </script>
-    </head>
-    <body>
-        <button onclick="oAuth2();"> 連結到 LineNotify 按鈕 </button>
-	</body>
-`
